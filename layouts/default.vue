7d5026a663013a0bfe4cacd87e632ed9c5f41162
Koyo Kisu <koyokisu@gmail.com>
2020-04-05 13:40:21 +0900

    contentful記事をトップに表示

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,48 +1,46 @@
 <template>
-  <div class="font-main flex flex-wrap justify-around">
-  <!-- ここからheader -->
-    <header class="w-64 h-64">
-      <div class="w-64 mt-24 ml-2 relative md:fixed">
-        <div class="flex">
-          <div class="bg-center bg-cover w-16 h-16 mr-2 rounded-full shadow photo"></div>
-          <div>
-            <h1 class="font-bold">Koyo</h1>
-            <p class="text-xs text-justify">self introduce area</p>
-          </div>
-        </div>
-        <div class="flex justify-around mt-5 text-xs">
-          <nuxt-link to="/">
-            <button>
-              <fa-layers full-width class="fa-2x block mx-auto mb-1">
-                <fa :icon="faCode" />
-              </fa-layers>
-              Works
-            </button>
-          </nuxt-link>
-          <a href="http://" target="_blank" rel="noopener noreferrer">
-            <button>
-              <fa-layers full-width class="fa-2x block mx-auto mb-1">
-                <fa :icon="faTwitter" />
-              </fa-layers>
-              Twitter
-            </button>
-          </a>
-          <a href="https://github.com/koyo-kisu" target="_blank" rel="noopener noreferrer">
-            <button>
-              <fa-layers full-width class="fa-2x block mx-auto mb-1">
-                <fa :icon="faGithub" />
-              </fa-layers>
-              GitHub
-            </button>
-          </a>
-        </div>
-      </div>
-    </header>
-    <!-- ここまでheader -->
-    <main class="w-full mt-10">
-      <nuxt />
-    </main>
-  </div>
+ <div class="font-main flex flex-wrap justify-around">
+   <header class="w-64 h-64">
+     <div class="w-64 mt-24 ml-2 relative md:fixed">
+       <div class="flex">
+         <div class="bg-center bg-cover w-16 h-16 mr-2 rounded-full shadow photo"></div>
+         <div>
+           <h1 class="font-bold">Koyo</h1>
+           <p class="text-xs text-justify">自己紹介</p>
+         </div>
+       </div>
+       <div class="flex justify-around mt-5 text-xs">
+         <nuxt-link to="/">
+          <button>
+            <fa-layers full-width class="fa-2x block mx-auto mb-1">
+              <fa :icon="faCode" />
+            </fa-layers>
+            Works
+          </button>
+        </nuxt-link>
+        <a href="https://twitter.com/koyo0824good" target="_blank" rel="noopener noreferrer">
+          <button>
+            <fa-layers full-width class="fa-2x block mx-auto mb-1">
+              <fa :icon="faTwitter" />
+            </fa-layers>
+            Twitter
+          </button>
+        </a>
+        <a href="https://github.com/koyo-kisu" target="_blank" rel="noopener noreferrer">
+          <button>
+            <fa-layers full-width class="fa-2x block mx-auto mb-1">
+              <fa :icon="faGithub" />
+            </fa-layers>
+            GitHub
+          </button>
+        </a>
+       </div>
+     </div>
+   </header>
+   <main class="w-full mt-10">
+     <nuxt />
+   </main>
+ </div>
 </template>
 
 <script>
